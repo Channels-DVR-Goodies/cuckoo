5548385b629964deea0414926070dd657455cd01
paul-chambers <projects@bod.org>
2021-05-08 14:30:31 -0700

    subdir scanning and execution of first-level executables.

--- cuckoo.c
+++ cuckoo.c
@@ -7,6 +7,8 @@
  * MIT Licensed
  */
 
+#define _GNU_SOURCE            1
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -18,6 +20,9 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <ftw.h>
 
 typedef struct {
     char * path;
@@ -25,60 +30,161 @@ typedef struct {
     char * name;
 } tSplitPath;
 
+const char * usageInstructions =
+{
+    "\n"
+    "Usage: cuckoo <pathname>"
+    "  Creates a subdirectory and moves the executable found at <pathname> into it.\n"
+    "  A symlink is then created at <pathname> that points to this executable.\n"
+    "  When this executable is invoked through the symlink, it goes through the\n"
+    "  subdirectory in alphabetical order, executing every executable it finds\n"
+    "  there, with the same command line parameters and environment it was\n"
+    "  invoked with through the symlink. This allows multiple executables to\n"
+    "  be executed transparently each time the original <pathname> executable\n"
+    "  would have been invoked before these changes were made.\n"
+    "\n"
+    "More information can be found at https://paul-chambers.github.io/cuckoo\n"
+};
+
 void usage( const char * format, ... )
 {
     va_list args;
     va_start( args, format );
 
     vfprintf( stderr, format, args );
+    fprintf( stderr, "%sn", usageInstructions );
 
     va_end( args );
 }
 
+/**
+ * @brief split path into directory and name
+ * @param path
+ */
 void splitPath( tSplitPath * path )
 {
-    /* we're going to split it, so copy it first */
-    path->directory = strdup( path->path );
-    path->name = NULL;
+    struct stat pathInfo;
 
-    char * lastSlash = NULL;
-    for ( char * p = path->directory; *p != '\0'; p++ )
+    stat( path->path, &pathInfo );
+    if ( S_ISDIR(pathInfo.st_mode ) )
     {
-        if ( *p == '/' )
-        {
-            lastSlash = p;
-        }
+        path->directory = path->path;
+        path->name = strdup( "" );
     }
-    if ( lastSlash != NULL )
+    else
     {
-        *lastSlash = '\0';
-        path->name = lastSlash + 1;
+        /* we're going to split it, so copy it first */
+        path->directory = strdup( path->path );
+        path->name = NULL;
+
+        char * lastSlash = NULL;
+        for ( char * p = path->directory; *p != '\0'; p++ )
+        {
+            if ( *p == '/' )
+            {
+                lastSlash = p;
+            }
+        }
+        if ( lastSlash != NULL )
+        {
+            *lastSlash = '\0';
+            path->name = strdup(lastSlash + 1);
+            path->directory = realloc( path->directory, (lastSlash - path->directory) + 1 );
+        }
     }
 }
 
-tSplitPath * getMyPath( const char * argv0 )
+/**
+ * @brief merge directory and name to create a path
+ * @param ->path filled in with absolute path
+ */
+void mergePath( tSplitPath * path )
+{
+    char temp[PATH_MAX];
+
+    if ( path != NULL )
+    {
+        /* if there's already a value, free it */
+        if ( path->path != NULL )
+        {
+            free( path->path );
+            path->path = NULL;
+        }
+
+        /* fix up the directory component, so it's an absolute path */
+        if ( path->directory == NULL )
+        {
+            /* no directory component, so use the current working directory */
+            path->directory = getcwd( NULL, 0 );
+        }
+        else if ( path->directory[0] != '/' )
+        {
+            /* existing relative path, so convert to absolute */
+            char * dir = path->directory;
+            path->directory = realpath( dir, NULL );
+            free( dir );
+        }
+
+        /* make sure that snprintf doesn't emit '(null)' */
+        if ( path->name == NULL )
+        {
+            path->name = strdup( "" );
+        }
+
+        snprintf( temp, sizeof(temp), "%s/%s", path->directory, path->name );
+        path->path = realpath( temp, NULL );
+    }
+}
+
+void freeSplitPath( tSplitPath ** pPath )
+{
+    tSplitPath * path = *pPath;
+    if ( path != NULL )
+    {
+        if (path->path != NULL)
+        {
+            free( path->path );
+            path->path = NULL;
+        }
+        if (path->directory != NULL)
+        {
+            free( path->directory );
+            path->directory = NULL;
+        }
+        if (path->name != NULL)
+        {
+            free( path->name );
+            path->name = NULL;
+        }
+        free( path );
+        *pPath = NULL;
+    }
+}
+
+tSplitPath * getArgv0Path(const char * argv0 )
 {
     tSplitPath * myPath = calloc( 1, sizeof(tSplitPath) );
 
     if (myPath != NULL)
     {
-        char * argv0copy = strdup( argv0 );
-        myPath->directory = realpath( dirname( argv0copy ), NULL);
-        free( argv0copy );
+        myPath->path = strdup( argv0 );
+        splitPath( myPath );
 
-        argv0copy = strdup( argv0 );
-        myPath->name = strdup( basename( argv0copy ) );
-        free( argv0copy );
+        char * p = myPath->directory;
+        myPath->directory = realpath( p, NULL );
+        free( p );
 
-        size_t size = strlen( myPath->directory ) + strlen( myPath->name ) + sizeof("/");
+        free( myPath->path );
+        size_t size = strlen( myPath->directory ) + strlen( myPath->name) + sizeof("/");
         myPath->path = malloc( size );
         if ( myPath->path != NULL )
         {
             snprintf( myPath->path, size, "%s/%s", myPath->directory, myPath->name );
         }
     }
-}
 
+    return myPath;
+}
 
 tSplitPath * getInstallPath( const char * relAppPath )
 {
@@ -99,6 +205,7 @@ tSplitPath * getInstallPath( const char * relAppPath )
             {
                 usage( "err: \'%s\' is not executable (%d: %s)\n",
                        result->path, errno, strerror( errno ) );
+
                 free( result->path);
                 free( result );
                 result = NULL;
@@ -180,14 +287,14 @@ int makeScriptsDir( tSplitPath * scriptsPath )
 const char * getPathToSelf( void )
 {
     /* figure out the absolute path to this executable */
-    char execPath[PATH_MAX + 1];
-    execPath[0] = '\0';
-    size_t len = readlink( "/proc/self/exe", execPath, sizeof(execPath));
-    if ( len > 0 )
+    char temp[PATH_MAX + 1];
+    temp[0] = '\0';
+    size_t len = readlink( "/proc/self/exe", temp, sizeof(temp));
+    if ( len >= 0 )
     {
-        execPath[len] = '\0';
+        temp[len] = '\0';
     }
-    return strdup( execPath );
+    return strdup( temp );
 };
 
 /**
@@ -232,11 +339,15 @@ int install( const char *relAppPath )
                              installPath->path, execPath, errno, strerror(errno));
                     return errno;
                 }
+                free( (void *)execPath );
             }
+            freeSplitPath( &scriptsPath );
         }
         fprintf( stderr, "Installed \'%s\' to \'%s\' successfully.\n"
                          "The script directory can be found at \'%s\'\n",
                  execPath, installPath->path, scriptsPath->directory );
+
+        freeSplitPath( &installPath );
     }
 
     /* we probably should free installPath, scriptsPath and execPath,
@@ -245,9 +356,131 @@ int install( const char *relAppPath )
     return result;
 }
 
-int invoke( tSplitPath * myPath, int argc, char * argv[], char * envp[] )
+/**
+ * @brief launches an executable
+ * @param argv array of arguments. argv[0] is path to executable. terminated by null pointer.
+ * @param envp array of environment values, terminated by null pointer.
+ * @return exit code from launched process.
+ */
+int launch( char * argv[], char * envp[] )
 {
     int result = 0;
+    int status;
+
+    /* `vfork()` is slightly more efficient than `fork()` for this common scenario, where the child
+     * immediately calls one of the variants of `execve()`, as it doesn't bother creating a copy of
+     * the page tables, only to blow them away immediately by calling `execve()` */
+    int pid = vfork();
+    switch ( pid )
+    {
+    case -1: /* fork failed */
+        syslog( LOG_ERR, "err: unable to launch \'%s\' (%d: %s)", argv[0], errno, strerror(errno) );
+        result = errno;
+        break;
+
+    case 0: /* this execution thread is the child */
+        execve( argv[0], argv, envp );
+        break; /* execve should never return... */
+
+    default: /* this execution thread is the parent - 'pid' is of the child */
+        waitpid( pid, &status, 0 );
+        if ( WEXITSTATUS( status ) )
+        {
+            result = WEXITSTATUS( status );
+        }
+        break;
+    }
+    return result;
+}
+
+typedef struct sExecutable {
+    struct sExecutable * next;
+    char                 path[1];
+} tExecutable;
+
+tExecutable * executableHead;
+
+int forEachEntry( const char * path, const struct stat *info, int entryType, struct FTW * ftw )
+{
+    int result = FTW_CONTINUE;
+
+    // fprintf( stderr, "level: %d, path: %s\n",  ftw->level, path );
+    switch (entryType)
+    {
+    case FTW_D:
+    case FTW_DP:
+    case FTW_DNR:
+        if ( ftw->level > 0 )
+        {
+            /* for any directory apart from the root one, don't decend into it */
+            result = FTW_SKIP_SUBTREE;
+        }
+        break;
+
+    case FTW_F:
+        /* we were given a file - is it executable? */
+        if ( faccessat( AT_FDCWD, path, X_OK, 0 ) == 0 )
+        {
+            int pathLen = strlen( path );
+            tExecutable * executable = calloc( 1, sizeof( tExecutable ) + pathLen );
+            if ( executable != NULL )
+            {
+                memcpy( executable->path, path, pathLen );
+                tExecutable ** prev = &executableHead;
+                tExecutable * exct = executableHead;
+                while ( exct != NULL )
+                {
+                    if ( strcoll(executable->path, exct->path) < 0 )
+                    {
+                        /* insewrt the new entry before this one */
+                        break;
+                    }
+                    prev = &exct->next;
+                    exct = exct->next;
+                }
+                executable->next = exct;
+                *prev = executable;
+            }
+        }
+        break;
+
+    default:
+        break;
+    }
+
+    return result;
+}
+
+int invoke( tSplitPath * myPath, char * argv[], char * envp[] )
+{
+    int result = 0;
+
+    tSplitPath *scriptsPath = getScriptsPath( myPath );
+    // fprintf( stderr, "directory %s\n", scriptsPath->directory );
+
+    executableHead = NULL;
+
+    nftw( scriptsPath->directory, forEachEntry, 2, FTW_ACTIONRETVAL );
+
+    tExecutable * executable = executableHead;
+    while ( executable != NULL )
+    {
+        argv[0] = executable->path;
+        int res = launch( argv, envp );
+        if ( result == 0 && res != 0 )
+        {
+            result = res;
+        }
+
+        /* done with this one, so free it */
+        tExecutable * f = executable;
+        executable = executable->next;
+        free( f );
+    }
+
+    executableHead = NULL;
+
+    freeSplitPath( &scriptsPath );
 
     return result;
 }
@@ -259,30 +492,35 @@ int main( int argc, char * argv[], char * envp[] )
 
     FILE * output = stdout;
 
-    tSplitPath * myPath = getMyPath( argv[0] );
+    tSplitPath * myPath = getArgv0Path( argv[0] );
 
-    openlog( myPath->name, LOG_PID, LOG_USER );
-
-
-    if ( strcmp( myPath->name, "cuckoo" ) == 0 )
+    if ( myPath != NULL)
     {
-        /* it's an install */
-        if ( argc != 2 || argv[1] == NULL || strlen( argv[1] ) < 1 )
+        openlog( myPath->name, LOG_PID, LOG_USER );
+
+
+        if ( strcmp( myPath->name, "cuckoo" ) == 0 )
         {
-            usage("please provide the path to the executable to intercept");
+            /* it's an install */
+            if ( argc != 2 || argv[1] == NULL || strlen( argv[1] ) < 1 )
+            {
+                usage("please provide the path to the executable to intercept");
+            }
+            else
+            {
+                result = install( argv[1] );
+            }
         }
         else
         {
-            result = install( argv[1] );
+            /* it's an invocation through a symlink */
+            result = invoke( myPath, argv, envp );
         }
-    }
-    else
-    {
-        /* it's an invocation through a symlink */
-        result = invoke( myPath, argc, argv, envp );
-    }
 
-    closelog();
+        freeSplitPath( &myPath );
+
+        closelog();
+    }
 
     return result;
 }
